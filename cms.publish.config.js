e2b435cb549cb98069abe2d914ab02173bfde198
吴建昆 <wujiankun@corp.netease.com>
2020-10-22 11:09:35 +0800

    添加发布配置文件

---
+++ cms.publish.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const fs = require('fs')
+const chalk = require('chalk')
+const symbols = require('log-symbols')
+const packageObj = require('./package.json')
+const channelName = packageObj.channel
+const project = packageObj.name
+
+const configPath = path.resolve('openID.json')
+if (!fs.existsSync(configPath)) {
+  console.log(symbols.error, chalk.red('没有在项目根目录中找到 openID.json '))
+  return
+}
+const account = require('./openID.json')
+const upPath = `f2e/sales/${account.gitName}/${channelName}/${project}`
+
+module.exports = {
+  entry: ['index.shtml'], // * or an array exp: ['./a.html', './module/b.html', './11/*.html']
+  output: {
+    cdnPath: 'https://static.ws.126.net/163/',
+    clean: true, // whether remove existing file in dist folder, before each building
+    dir: { root: 'build' }
+  },
+  svnRoot: account.svnRoot, // setting svn root dir for compatible with absolute paths of previous projects
+  log: true,
+  image: {
+    base64: 1000 // less than 1000 byte, would convert to base64 url
+  },
+  upload: {
+    username: account.username,
+    password: account.password,
+    path: upPath,
+    includeHtml: false,
+    exclude: []
+    // otppwd: '754335' // netease token is used in outer network env
+  },
+  api: {
+    cms: account.cms
+  }
+}
